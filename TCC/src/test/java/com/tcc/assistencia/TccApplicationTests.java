3ab5d21cd58729bc35ef2a9315381135d1244424
BrainWave3202 <BrainWave3202@gmail.com>
2023-04-09 19:04:36 -0300

    Criação do projeto

---
+++ TCC/src/test/java/com/tcc/assistencia/TccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.assistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
